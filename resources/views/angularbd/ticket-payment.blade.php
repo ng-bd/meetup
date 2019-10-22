280085b13cd09f56c5da6e28d34dc315e8a8196d
Foysal Zafree <zafree@deligram.com>
2019-10-22 16:38:52 +0600

    add payment markup

--- resources/views/angularbd/ticket-payment.blade.php
+++ resources/views/angularbd/ticket-payment.blade.php
@@ -1,19 +1,45 @@
 @extends('angularbd.sub-layout')
-
-@section('content')
-    <div class="loveMeLikeYouDo">
-        Name: {{ $attendee->name }}<br>
-        Email: {{ $attendee->email }}<br>
-        Phone: {{ $attendee->mobile }}<br>
-        T-Shirt: {{ data_get($attendee, 'misc.tshirt') }}<br>
-        Ticket Price : {{ env('EVENT_TICKET_PRICE') }}<br>
-    </div>
-{!!
-    aamarpay_post_button([
-        'cus_name'  => $attendee->name,
-        'cus_email' => $attendee->email,
-        'cus_phone' => $attendee->mobile,
-        'opt_a' => $attendee->id,
-    ], env('EVENT_TICKET_PRICE'), 'Pay Now', 'Button Button--submit')
-!!}
-@endsection
+<main class="Meetup__payment">
+    @section('content')
+        <h4 class="Meetup__sectionTitle">Ticket Price {{ env('EVENT_TICKET_PRICE') }}tk.</h4>
+        <p class="Meetup__sectionCopy">Our payment partner aamarPay is an Online Payment Gateway & Merchant Service Provider of Bangladesh. Aiming to provide best payment experience that an estore or customer can expect from a payment processor company.</p>
+        <div class="Meetup__suspect">
+            <div class="Meetup__suspectCopy">
+                <span>Name</span>
+                <span>{{ $attendee->name }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>Email</span>
+                <span>{{ $attendee->email }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>Phone</span>
+                <span>{{ $attendee->mobile }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>Profession</span>
+                <span>{{ $attendee->profession }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>Link</span>
+                <span>{{ $attendee->social_profile_url }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>T-shirt</span>
+                <span>{{ data_get($attendee, 'misc.tshirt') }}</span>
+            </div>
+            <div class="Meetup__suspectCopy">
+                <span>Note</span>
+                <span>You wrote nothing.</span>
+            </div>
+        </div>
+    {!!
+        aamarpay_post_button([
+            'cus_name'  => $attendee->name,
+            'cus_email' => $attendee->email,
+            'cus_phone' => $attendee->mobile,
+            'opt_a' => $attendee->id,
+        ], env('EVENT_TICKET_PRICE'), 'Pay 250tk.', 'Button Button--submit')
+    !!}
+    @endsection
+</main>
