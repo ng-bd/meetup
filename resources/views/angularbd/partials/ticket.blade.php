d088642f8aa5aad08ad14209b2c7e814f58f13c8
Foysal Zafree <zafree@deligram.com>
2019-10-22 12:16:57 +0600

    update markup

--- resources/views/angularbd/partials/ticket.blade.php
+++ resources/views/angularbd/partials/ticket.blade.php
@@ -1,79 +1,85 @@
 <main class="Meetup__ticket">
-    <form class="form-contact buy-ticket" action="{{ route('buy.ticket.post') }}" method="post" id="buyTicket">
+    <form class="Meetup__form" action="{{ route('buy.ticket.post') }}" method="post" id="buyTicket">
         @csrf
         <input type="hidden" name="type" value="{{ \App\Enums\AttendeeType::ATTENDEE }}">
-        <div class="row">
-            <div class="col-12">
-                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <input value="{{ old('name') }}" class="form-control" name="name" id="name" type="text" placeholder="Enter your name">
-                    <small class="text-danger">{{ $errors->first('name') }}</small>
-                </div>
-            </div>
-
-            <div class="col-sm-6 user_detail">
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input value="{{ old('email') }}" class="form-control" name="email" id="email" type="email" placeholder="Enter email address">
-                    <small class="text-danger">{{ $errors->first('email') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group{{ $errors->has('mobile') ? ' has-error' : '' }}">
-                    <input value="{{ old('mobile') }}" class="form-control" name="mobile" id="mobile" type="text" placeholder="Enter phone number">
-                    <small class="text-danger">{{ $errors->first('mobile') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group {{ $errors->has('profession') ? ' has-error' : '' }}">
-                    <input class="form-control" name="profession" id="profession" type="text" placeholder="Enter your profession"
-                           value="{{ old('profession') }}"
-                    />
-                    <small class="text-danger">{{ $errors->first('profession') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group {{ $errors->has('social_profile_url') ? ' has-error' : '' }}">
-                    <input class="form-control" name="social_profile_url" id="social_profile_url" type="text" placeholder="Enter your social profile url"
-                           value="{{ old('social_profile_url') }}"
-                    />
-                    <small class="text-danger">{{ $errors->first('social_profile_url') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group {{ $errors->has('misc.tshirt') ? ' has-error' : '' }}">
-                    <select class="form-control" name="misc[tshirt]" id="tshirt" placeholder="T-shirt Size">
-                        <option value="">Select Your T-shirt size</option>
-                        @foreach(trans('t_shirt') as $key => $tShirt)
-                            <option value="{{ $tShirt }}" @if(old('misc.tshirt') == $tShirt) selected @endif> {{ $tShirt }} </option>
-                        @endforeach
-                    </select>
-                    <small class="text-danger">{{ $errors->first('misc.tshirt') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group {{ $errors->has('misc.working') ? ' has-error' : '' }}">
-                    <select class="form-control" name="misc[working]" placeholder="Working with Angular?">
-                        <option value="0">Never Heard of it</option>
-                        <option value="1">1 Year</option>
-                        <option value="2">2 Year</option>
-                        <option value="3">3 Year</option>
-                        <option value="4">4 Year</option>
-                        <option value="4+">4+ Year</option>
-                    </select>
-                    <small class="text-danger">{{ $errors->first('misc.working') }}</small>
-                </div>
-            </div>
-            <div class="col-sm-6 user_detail">
-                <div class="form-group {{ $errors->has('misc.instruction') ? ' has-error' : '' }}">
-                    <textarea name="instruction">
-
-                    </textarea>
-                    <small class="text-danger">{{ $errors->first('misc.instruction') }}</small>
-                </div>
-            </div>
+        <div class="Field {{ $errors->has('name') ? ' Field--error' : '' }}">
+          <label class="Field__label">Name</label>
+          <div class="Field__control">
+            <input class="Field__input" value="{{ old('name') }}" name="name" id="name" type="text" placeholder="Write your name">
+          </div>
+          @if($errors->has('name'))
+            <p class="Field__validation">{{ $errors->first('name') }}</p>
+          &endif
         </div>
-        <div class="form-group mt-3 user_detail">
-            <button type="submit" class="button button-contactForm">Submit</button>
+        <div class="Field {{ $errors->has('email') ? ' Field--error' : '' }}">
+          <label class="Field__label">Email</label>
+          <div class="Field__control">
+            <input class="Field__input" value="{{ old('email') }}" name="email" id="email" type="email"  placeholder="Write your email address">
+          </div>
+          @if($errors->has('email'))
+            <p class="Field__validation">{{ $errors->first('email') }}</p>
+          &endif
+        </div>
+        <div class="Field {{ $errors->has('mobile') ? ' Field--error' : '' }}">
+          <label class="Field__label">Phone</label>
+          <div class="Field__control">
+            <input class="Field__input" value="{{ old('mobile') }}" name="mobile" id="mobile" type="number" placeholder="Enter phone number">
+          </div>
+          @if($errors->has('mobile'))
+            <p class="Field__validation">{{ $errors->first('mobile') }}</p>
+          &endif
+        </div>
+        <div class="Field {{ $errors->has('profession') ? ' Field--error' : '' }}">
+          <label class="Field__label">Profession</label>
+          <div class="Field__control">
+            <input class="Field__input" value="{{ old('profession') }}" name="profession" id="profession" type="text" placeholder="Write your profession">
+          </div>
+          @if($errors->has('profession'))
+            <p class="Field__validation">{{ $errors->first('profession') }}</p>
+          &endif
+        </div>
+        <div class="Field {{ $errors->has('social_profile_url') ? ' Field--error' : '' }}">
+          <label class="Field__label">Social profile link</label>
+          <div class="Field__control">
+            <input class="Field__input" value="{{ old('social_profile_url') }}" name="social_profile_url" id="social_profile_url" type="text" placeholder="Enter your social profile url">
+          </div>
+          @if($errors->has('social_profile_url'))
+            <p class="Field__validation">{{ $errors->first('social_profile_url') }}</p>
+          &endif
+        </div>
+        <div class="Field {{ $errors->has('misc.working') ? ' Field--error' : '' }}">
+          <label class="Field__label">How long you are working with Angular?</label>
+          <div class="Field__control Field__control--select">
+            <select class="Field__input" name="misc[working]" id="working">
+              <option value="" selected hidden>Select here</option>
+              <option value="">Never Heard of it</option>
+              <option value="">1 Year</option>
+              <option value="">2 Years</option>
+              <option value="">3 Years</option>
+              <option value="">4 Years</option>
+              <option value="">4+ Years</option>
+            </select>
+          </div>
+          @if($errors->has('misc.working'))
+            <p class="Field__validation">{{ $errors->first('misc.working') }}</p>
+          &endif
+        </div>
+        <div class="Field {{ $errors->has('misc.tshirt') ? ' Field--error' : '' }}">
+          <label class="Field__label">T-shirt size</label>
+          <div class="Field__control Field__control--select">
+            <select class="Field__input" name="misc[tshirt]" id="tshirt">
+              <option value="" selected hidden>Select your t-shirt size</option>
+              @foreach(trans('t_shirt') as $key => $tShirt)
+                <option value="{{ $tShirt }}" @if(old('misc.tshirt') == $tShirt) selected @endif>{{ $tShirt }}</option>
+              @endforeach
+            </select>
+          </div>
+          @if($errors->has('misc.tshirt'))
+            <p class="Field__validation">{{ $errors->first('misc.tshirt') }}</p>
+          &endif
+        </div>
+        <div class="Field Field--block">
+          <button type="submit" class="Button Button--submit">Submit</button>
         </div>
     </form>
-
 </main>
