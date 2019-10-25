017766d983fd6de848d89e555d2c0dea5cffc5bc
M A Hossain Tonu <mahtonu@gmail.com>
2019-10-26 04:20:20 +0600

    speakers list added

--- resources/views/angularbd/partials/speakers.blade.php
+++ resources/views/angularbd/partials/speakers.blade.php
@@ -1,23 +1,66 @@
 <div class="Meetup__speakers" id="speakers">
     <h3 class="Meetup__sectionTitle">Meet Our Speakers</h3>
     <div class="Speaker">
-        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/speaker.svg') }}" alt="speaker"></div>
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/ziaul-haq.jpg') }}" alt="speaker" style="border-radius: 40px"></div>
         <div class="Speaker__content">
-            <h5 class="Speaker__name">Kuddus Boyati</h5>
-            <p class="Speaker__role">Music Engineer</p>
-            <p class="Speaker__bio">Kuddus was born in 1947 at Rajibpur village, Kendua Upazila of Netrokona
-                District during the time of the British Raj. Due to financial problem, he didn't have the
-                opportunity to study and started music practice at the age of 11.</p>
+            <h5 class="Speaker__name">Md. Ziaul Haq</h5>
+            <p class="Speaker__role">Head of Web Engineering, Pathao  </p>
+            <p class="Speaker__bio">Ziaul Haq, is a Software Architect, JavaScript Enthusiast, Engineering Manager, Speaker, Certified Scrum Master and Author. He has written a book on ‘Angular Test Driven Development’ from packt.
+
+            Ziaul is currently leading the web engineering team at Pathao. Before joining Pathao, he has worked for a long time with Upwork core development team as JavaScript Engineer. </p>
         </div>
     </div>
     <div class="Speaker">
-        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/speaker.svg') }}" alt="speaker"></div>
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/sm-asad.jpg') }}" alt="speaker" style="border-radius: 40px"></div>
         <div class="Speaker__content">
-            <h5 class="Speaker__name">Kuddus Boyati</h5>
-            <p class="Speaker__role">Music Engineer</p>
-            <p class="Speaker__bio">Kuddus was born in 1947 at Rajibpur village, Kendua Upazila of Netrokona
-                District during the time of the British Raj. Due to financial problem, he didn't have the
-                opportunity to study and started music practice at the age of 11.</p>
+            <h5 class="Speaker__name">S M Asad Rahman</h5>
+            <p class="Speaker__role">CTO, Monstar Lab Bangladesh</p>
+            <p class="Speaker__bio">As a Full Stack Developer, over 9 years of Software Engineering experience in several LAMP projects. He is interested to engage himself in programming, database designing and building effective architecture of software to ensure performance, security and modifiability. Also identifying design risks and mitigate them early in the development process.</p>
+        </div>
+    </div>
+    <div class="Speaker">
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/mahtonu.jpg') }}" alt="speaker" style="border-radius: 40px"></div>
+        <div class="Speaker__content">
+            <h5 class="Speaker__name">M A Hossain Tonu</h5>
+            <p class="Speaker__role">Software Development Lead, Vantage Labs Dhaka.</p>
+            <p class="Speaker__bio">A passionate developer since last 13 years & the author of the book “PHP Application Development with NetBeans: Beginner's Guide”. He is a Certified ScrumMaster®, Certified Scrum Professional®, and working at Vantage Labs, Dhaka (http://www.vantage.com/) where he is leading and maintaining a highly available SAAS platforms like Amped (https://amped.ai) that is the single most intuitive and easy to use Customer Relationship Management system on the market and Delyte (https://delyte.ai) that is the new generation Customer Support for any company support reps. He has been worked with Public Health Service & Solutions (PHSS),
+            http://www.thephss.org as a Research and Development Lead for three years and designed & led a good number of software products/teams for Nigeria’s National Polio Eradication Program with World Health Organization (WHO) and Bill & Melinda Gates Foundation.
+            </p>
+        </div>
+    </div>
+    <div class="Speaker">
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/shihan.jpg') }}" alt="speaker" style="border-radius: 40px"></div>
+        <div class="Speaker__content">
+            <h5 class="Speaker__name">Mohammad Nurul Islam (Shihan)</h5>
+            <p class="Speaker__role">Technical Board Advisor, Deligram Technology Ltd.</p>
+            <p class="Speaker__bio">Shihan is a full stack Software Architect, specialised in web & server technologies. In his 20+ years of career, he has worked with many languages, tools & technologies, that made him a perfect polyglot in software development. Starting with raw JS for rich frontend development, he has worked with almost all popular JS libraries & frameworks, including jQuery, Backbone, Angular, React & Vue. He is a constant practitioner/advocate of reusable patterns & best practices in software development, open source enthusiast, hacker and pure community guy. In his free time, he loves to spend time with his family, hangout with other engineers and occasionally share his knowledge in community conferences/meetups.</p>
+        </div>
+    </div>
+    <div class="Speaker">
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/speaker.svg') }}" alt="speaker" style="border-radius: 40px"></div>
+        <div class="Speaker__content">
+            <h5 class="Speaker__name">Speaker </h5>
+            <p class="Speaker__role">Title</p>
+            <p class="Speaker__bio">Lorem ipsum...
+            </p>
+        </div>
+    </div>
+    <div class="Speaker">
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/speaker.svg') }}" alt="speaker" style="border-radius: 40px"></div>
+        <div class="Speaker__content">
+            <h5 class="Speaker__name">Speaker </h5>
+            <p class="Speaker__role">Title</p>
+            <p class="Speaker__bio">Lorem ipsum...
+            </p>
+        </div>
+    </div>
+    <div class="Speaker">
+        <div class="Speaker__avatar"><img src="{{ asset('angularbd/images/speaker.svg') }}" alt="speaker" style="border-radius: 40px"></div>
+        <div class="Speaker__content">
+            <h5 class="Speaker__name">Speaker </h5>
+            <p class="Speaker__role">Title</p>
+            <p class="Speaker__bio">Lorem ipsum...
+            </p>
         </div>
     </div>
 </div>
