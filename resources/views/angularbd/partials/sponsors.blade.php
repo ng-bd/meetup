cb9cd4caa912e85246efd3c48fa9396b655cdea5
M A Hossain Tonu <mahtonu@gmail.com>
2019-10-28 02:33:37 +0600

    sponsors url updated

--- resources/views/angularbd/partials/sponsors.blade.php
+++ resources/views/angularbd/partials/sponsors.blade.php
@@ -3,13 +3,14 @@
     <div class="Meetup__sponsorsWrapper">
         <a class="Meetup__sponsor" href="https://monstar-lab.com/global" target="_blank"><img src="{{ asset('angularbd/images/sponsor-1@2x.png') }}" alt="sponsor"></a>
         <a class="Meetup__sponsor" href="https://www.vantage.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-2@2x.png') }}" alt="sponsor"></a>
-        <a class="Meetup__sponsor" href="https://www.techmastersbd.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-3@2x.png') }}" alt="sponsor"></a>
-        <a class="Meetup__sponsor" href="https://thesoftking.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-4@2x.png') }}" alt="sponsor"></a>
+        <a class="Meetup__sponsor" href="https://business.eskimi.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-3@2x.png') }}" alt="sponsor"></a>
+        <a class="Meetup__sponsor" href="https://www.techmastersbd.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-4@2x.png') }}" alt="sponsor"></a>
         <a class="Meetup__sponsor" href="http://vivasoftltd.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-5@2x.png') }}" alt="sponsor"></a>
-        <a class="Meetup__sponsor" href="https://jouleslabs.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-6@2x.png') }}" alt="sponsor"></a>
+        <a class="Meetup__sponsor" href="https://thesoftking.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-6@2x.png') }}" alt="sponsor"></a>
+        <a class="Meetup__sponsor" href="https://jouleslabs.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-7@2x.png') }}" alt="sponsor"></a>
     </div>
     <h4 class="Meetup__sectionTitle">Payment Partner</h4>
     <div class="Meetup__sponsorsWrapper">
-        <a class="Meetup__sponsor" href="https://aamarpay.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-7@2x.png') }}" alt="sponsor"></a>
+        <a class="Meetup__sponsor" href="https://aamarpay.com/" target="_blank"><img src="{{ asset('angularbd/images/sponsor-8@2x.png') }}" alt="sponsor"></a>
     </div>
 </div>
