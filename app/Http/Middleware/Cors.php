af5cc7c37be0d876769876d1a08388cfe4d88ec6
Shipu Ahamed <shipu@deligram.com>
2019-11-02 02:43:25 +0600

    Add cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $httpOrigin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : false;
+        $allowedOrigins = [];
+        if(in_array($httpOrigin, $allowedOrigins)) {
+            return $next($request)->header('Access-Control-Allow-Origin' , '*')
+                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+        }
+
+        return $next($request);
+    }
+}
